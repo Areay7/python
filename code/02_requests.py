30f43c551523df0ed2934bfcab900fe77dfff61f
Areay7 <2507549477@qq.com>
2022-12-19 23:41:03 +0800

    获取1960年-2019年全球前10GDP数据

--- code/02_requests.py
+++ code/02_requests.py
@@ -2,11 +2,7 @@
 import requests
 
 # 2.发送请求,获取响应
-<<<<<<< HEAD
 responese = requests.get('https://ncov.dxy.cn/ncovh5/view/pneumonia')
-=======
-responese = requests.get('https://areay7.github.io')
->>>>>>> 14b35f7 (获取1960年-2019年全球前10GDP数据)
 
 
 # 3.从响应中,获取响应数据
